472455e1106d6c3ff5dd3ba09a8fe5a6cad6137b
Till Kolter <till.kolter@gmail.com>
2017-06-10 15:26:09 +0500

    show saved credit cards

---
+++ components/checkout/CardPaymentMethod.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="card__holder">Holder Name: {{ data.holderName }}</div>
+    <div class="card__number">Number: **** **** **** {{ data.number }}</div>
+    <div class="card__expiry">Expiry: {{ data.expiryMonth }}/{{ data.expiryYear}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CardPaymentMethod',
+    props: ['data']
+  }
+</script>
--- components/checkout/CheckoutPayment.vue
+++ components/checkout/CheckoutPayment.vue
@@ -3,15 +3,26 @@
     <div class="row">
       <div class="col-12 checkout__content__col">
         <h3>Choose payment method</h3>
-        <template v-for="(option, i) in paymentOptions">
-          <div class="radio" v-show="showOption(option)">
-            <label>
-              <input type="radio" name="payment" v-model="selectedPayment" :value="option">
-              <span>{{option.name}}</span>
-              <img :src="image" v-for="(image, j) in option.logos"/>
-            </label>
+        <div class="row">
+          <div :class="[availableMethods ? 'col-6': 'col-12']">
+            <template v-for="(option, i) in paymentOptions">
+              <div class="radio" v-show="showOption(option)">
+                <label>
+                  <input type="radio" name="payment" v-model="selectedPayment" :value="option">
+                  <span>{{option.name}}</span>
+                  <img :src="image" v-for="(image, j) in option.logos"/>
+                </label>
+              </div>
+            </template>
           </div>
-        </template>
+          <div v-if="availableMethods" class="col-6">
+            <h4>Saved payment methods</h4>
+            <div class="radio checkout__payment__method" v-for="(method, i) in availableMethods">
+              <input type="radio" name="payment-method" v-model="selectedPaymentMethod" :value="method">
+              <component :key="`paymentMethod-${selectedPayment.id}-${i}`" :is="paymentMethodComponent(selectedPayment)" :data="getPaymentData(selectedPayment, method)" ></component>
+            </div>
+          </div>
+        </div>
         <proceed-button @click="onProceed" :class="['proceed-btn-payment', 'float-right-bottom', !selectedPayment ? 'disabled': '']">Review your order</proceed-button>
       </div>
     </div>
@@ -24,13 +35,15 @@
   import * as types from '../../store/types'
   import ProceedButton from '../shared/ProceedButton'
   import { mastercardLogoSmall, paypalLogoSmall, sofortLogoSmall, visaLogoSmall } from '../../utils/logos'
+  import CardPaymentMethod from '~/components/checkout/CardPaymentMethod'
 
   export default {
     components: {ProceedButton},
     name: 'CheckoutPayment',
     data: function () {
       return {
-        selectedPayment: null
+        selectedPayment: null,
+        selectedPaymentMethod: null
       }
     },
     computed: {
@@ -42,6 +55,9 @@
       },
       shippingOption () {
         return this.$store.getters.getShippingOption
+      },
+      availableMethods () {
+        return this.selectedPayment && this.selectedPayment.methods
       }
     },
     methods: {
@@ -52,6 +68,17 @@
               id
               name
               logos
+              methods {
+                reference
+                ... on CardMethodType {
+                  cardData {
+                    number
+                    expiryMonth
+                    expiryYear
+                    holderName
+                  }
+                }
+              }
             }
           }
           `,
@@ -93,6 +120,16 @@
           }
         }
       },
+      paymentMethodComponent (paymentOption) {
+        if (paymentOption.id === 'creditcard') {
+          return CardPaymentMethod
+        }
+      },
+      getPaymentData (paymentOption, paymentMethod) {
+        if (paymentOption.id === 'creditcard') {
+          return paymentMethod.cardData
+        }
+      },
       showOption (option) {
         let isPickupOption = this.shippingOption && this.shippingOption.id === '-1'
         if (option.id === 'forward' && isPickupOption) {
@@ -113,6 +150,9 @@
       },
       selectedPayment (payment) {
         this.$store.commit(types.SET_SELECTED_PAYMENT_OPTION, payment)
+        if (this.availableMethods) {
+          this.selectedPaymentMethod = this.availableMethods[0]
+        }
       }
     },
     mounted () {
