3ab409ecf8eb539e8b3aab064917ed1db418a274
Till Kolter <till.kolter@gmail.com>
2017-06-13 15:43:01 +0500

    google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -116,7 +116,8 @@ module.exports = {
     {src: '~plugins/vue-cookie', ssr: false, injectAs: 'cookie'},
     {src: '~plugins/apollo.js', injectAs: 'apolloProvider'},
     {src: '~plugins/vue-resource'},
-    {src: '~plugins/vue-social-sharing'}
+    {src: '~plugins/vue-social-sharing'},
+    {src: '~plugins/ga.js', ssr: false }
   ],
   router: {
     middleware: ['check-auth', 'check-unpaid', 'payment-duty'],
