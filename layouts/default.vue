beade404987c7c22b01ff0c424f6c5a3eb87fe5a
Till Kolter <till.kolter@gmail.com>
2017-06-14 11:56:43 +0500

    embed the script inside a div

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
     <!-- a placeholder for the google analytics script -->
-    <!--<script>-->
-      <!--// eslint-disable-next-line no-unused-vars-->
-      <!--var nothing = function () {-->
-        <!--console.log('never called. never do nothing')-->
-      <!--}-->
-    <!--</script>-->
+    <div>
+      <script>
+        // eslint-disable-next-line no-unused-vars
+        var nothing = function () {
+          console.log('never called. never do nothing')
+        }
+      </script>
+    </div>
     <account-navbar></account-navbar>
     <header class="navbar" role="navigation">
       <brand-navbar :isOpenMobile="isOpenMobileMenu" v-on:togglemenu="onToggleMobileMenu"
