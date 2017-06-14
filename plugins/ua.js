6fdb834a192dfbad715d601a34d7960b79f4c60b
Till Kolter <till.kolter@gmail.com>
2017-06-14 14:56:46 +0500

    initialize on Vue.use and set router inside function

--- plugins/ua.js
+++ plugins/ua.js
@@ -5,16 +5,13 @@ import VueAnalytics from 'vue-ua'
 Vue.use(VueAnalytics, {
   appName: 'Oye Kolter', // Mandatory
   appVersion: '1.0', // Mandatory
-  trackingId: 'UA-100941329-1' // Mandatory
+  trackingId: 'UA-100941329-1', // Mandatory
+  debug: true // Whether or not display console logs debugs (optional)
 })
 
 export default ({ app, store }) => {
-  app.ua = new VueAnalytics({
-    appName: 'Oye Kolter', // Mandatory
-    appVersion: '1.0', // Mandatory
-    trackingId: 'UA-100941329-1', // Mandatory
-    debug: true, // Whether or not display console logs debugs (optional)
-    vueRouter: app.router // , // Pass the router instance to automatically sync with router (optional)
+  VueAnalytics.vueRouter = app.router
+  app.ua = VueAnalytics
     // ignoredViews: ['homepage'], // If router, you can exclude some routes name (case insensitive) (optional)
     // globalDimensions: [ // Optional
     //   {dimension: 1, value: 'MyDimensionValue'},
@@ -24,5 +21,4 @@ export default ({ app, store }) => {
     //   {metric: 1, value: 'MyMetricValue'},
     //   {metric: 2, value: 'AnotherMetricValue'}
     // ]
-  })
 }
