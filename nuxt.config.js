b8f6bac16428fd58b1a4f12aa692294cb78832d6
Till Kolter <till.kolter@gmail.com>
2017-07-02 17:47:32 +0200

    make API confirgurable

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,13 +6,11 @@ global.fetch = fetch
 
 var utils = require('./build/utils')
 
-var apiHost = "'http://localhost:8000'"
+var apiHost = process.env.API_ROOT
 
 if (!process.env.NODE_ENV && process.env.npm_lifecycle_event !== 'dev') {
   process.env.NODE_ENV = 'production'
 }
-
-console.log('NODE_ENV ' + process.env.NODE_ENV)
 
 var setupAPI = function () {
   apiHost = "'http://localhost:8000'"
@@ -40,7 +38,9 @@ var setupAPI = function () {
   }
 }
 
-setupAPI()
+if (!apiHost) {
+  setupAPI()
+}
 
 module.exports = {
   /*
