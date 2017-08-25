8452ec81f8d3ebdf42d14ce44a4a2432d8a0b71b
Till Kolter <till.kolter@gmail.com>
2017-08-25 08:45:12 +0200

    Make customer info design more clean

--- components/info/Privacy.vue
+++ components/info/Privacy.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="maincont">
-    <div class="mainhead">OYE Records: Privacy Policy</div>
-    <div class="order1 cont"><div class="nummer">1. 	</div><div class="inhalt">General Information</div></div>
-    <div class="order2 cont"><div class="nummer">1.1. </div><div class="inhalt">This Privacy Policy has been designed to inform you and notify you of the kinds, the scope and the purpose, as well as your rights concerning your data collected and processed within the scope of this website provided by Oye Records UG (haftungsbeschr&auml;nkt), Oderbergerstr. 4, D 10435 Berlin, Germany (hereinafter also referred to as "Provider") as data processor. </div></div>
-    <div class="order2 cont"><div class="nummer">1.2. </div><div class="inhalt">"You" and "your" refer to you, as a user of the website. "We", "us", and "our" refer to the Provider</div></div>
-    <div class="order1 cont"><div class="nummer">2. 	</div><div class="inhalt">Data processing and data security</div></div>
-    <div class="order2 cont"><div class="nummer">2.1. </div><div class="inhalt">We will collect, process and use personally identifiable data beyond the scope already permitted by law and without your required further consent only to the extent necessary for the conclusion and execution of a contractual relationship, performing requests or for billing purposes. </div></div>
-    <div class="order2 cont"><div class="nummer">2.2. </div><div class="inhalt">To protect your personally identifiable data collected and processed by us from unauthorized access and abuse, we have put a number of technical and organizational security measures in place, like encoding of privacy sensitive information (i.e. login data) transferred between this website and our servers.</div></div>
-    <div class="order2 cont"><div class="nummer">2.3. </div><div class="inhalt">These security measures are monitored on a regular basis and updated according to technical progress as necessary. Our staff is well informed about privacy rules and obliged to follow them. </div></div>
-    <div class="order1 cont"><div class="nummer">3. 	</div><div class="inhalt">Collection, Processing and Use of Personally Identifiable Data</div></div>
-    <div class="order2 cont"><div class="nummer">3.1. </div><div class="inhalt">Your personal data are used for the following purposes:</div></div>
-    <div class="order3 cont"><div class="nummer">-    </div><div class="inhalt">The provision of our website and the acceptance, processing and execution of your orders.</div></div>
-    <div class="order3 cont"><div class="nummer">-    </div><div class="inhalt">Ensuring an efficient customer service and / or technical support for your products. When you contact us your details will be stored in order to process the enquiry and in case additional questions arise.</div></div>
-    <div class="order3 cont"><div class="nummer">-    </div><div class="inhalt">Technical notifications concerning our service.</div></div>
-    <div class="order2 cont"><div class="nummer">3.2. </div><div class="inhalt">We pass your data on to third parties where this is necessary for invoicing purposes (e.g. carrying out transactions) or for other reasons to enable us to fulfil our contractual obligations to you. Should such passing on not be clear to you, we will point it out to you.</div></div>
-    <div class="order1 cont"><div class="nummer">4. 	</div><div class="inhalt">Newsletter</div></div>
-    <div class="order2 cont"><div class="nummer">4.1. </div><div class="inhalt">Newsletters and other emails that are not service-related, are not fulfilling our duties to perform our service and contain advertisement will only be send to you, if you have given us your prior consent.</div></div>
-    <div class="order2 cont"><div class="nummer">4.2. </div><div class="inhalt">The content of the newsletter or email follows from its respective description. If no description is available, the content sent comprises information about us, our products and our services.</div></div>
-    <div class="order2 cont"><div class="nummer">4.3. </div><div class="inhalt">If you no longer want to receive our newsletter or e-mails you can opt to stop receiving it at any time.</div></div>
-    <div class="order2 cont"><div class="nummer">4.4. </div><div class="inhalt">You also give us the consent to store your IP addresses as part of the sign-up process. We are legally bound to record the log-on process so as to be able to verify that logging on is done in accordance with regulations.</div></div>
-    <div class="order1 cont"><div class="nummer">5. 	</div><div class="inhalt">Cookies and similar features</div></div>
-    <div class="order2 cont"><div class="nummer">5.1. </div><div class="inhalt">A cookie (or similar data medium) is a small piece of data stored in a user's device while the user is using a website. Cookies can be send back to a server any time a user loads the website or accesses certain functions.</div></div>
-    <div class="order2 cont"><div class="nummer">5.2. </div><div class="inhalt">We apply cookies only in order to support the functions of this website, optimize your experience as its user.</div></div>
-    <div class="order2 cont"><div class="nummer">5.3. </div><div class="inhalt">We will ask for your consent or point out to the fact that we store and read cookies on your device.</div></div>
-    <div class="order2 cont"><div class="nummer">5.4. </div><div class="inhalt">You may opt-out from many ad-serving cookies by using the services provided by http://www.aboutads.info/choices/ or http://www.youronlinechoices.com/uk/your-ad-choices/ for users form the European Union.</div></div>
-    <div class="order1 cont"><div class="nummer">6.		</div><div class="inhalt">Service and Notices</div></div>
-    <div class="order2 cont"><div class="nummer">6.1. </div><div class="inhalt">In compliance with legal requirements we will at any time provide you with information on your personally identifiable data stored by us, free of charge and without undue delay. </div></div>
-    <div class="order2 cont"><div class="nummer">6.2. </div><div class="inhalt">Furthermore, you are free to revoke the approvals granted by confirming this Privacy Policy, at any time and with future effect. If you wish to do so, please feel free to contact us. Contact information is provided in Number 1 in this Privacy Policy.</div></div>
-    <div class="order1 cont"><div class="nummer">7. 	</div><div class="inhalt">Amendments of this Privacy Policy</div></div>
-    <div class="order2 cont"><div class="nummer">7.1. </div><div class="inhalt">We reserve the right to modify or amend this Privacy Policy with view to the provision of new services, if and to the extent necessary. The same applies if the current Privacy Policy is found to contain any gaps. </div></div>
-    <div class="order2 cont"><div class="nummer">7.2. </div><div class="inhalt">Upon considerable amendments and if your consent is needed, we will contact you by email sent to the email address provided to us.</div></div>
+  <div class="privacy">
+  <h3>1. General Information</h3>
+  <p><strong>1.1. </strong>This Privacy Policy has been designed to inform you and notify you of the kinds, the scope and the purpose, as well as your rights concerning your data collected and processed within the scope of this website provided by Oye Records UG (haftungsbeschr&auml;nkt), Oderbergerstr. 4, D 10435 Berlin, Germany (hereinafter also referred to as "Provider") as data processor.</p>
+    <p><strong>1.2. </strong>"You" and "your" refer to you, as a user of the website. "We", "us", and "our" refer to the Provider</p>
+    <h3>2. Data processing and data security</h3>
+    <p><strong>2.1. </strong>We will collect, process and use personally identifiable data beyond the scope already permitted by law and without your required further consent only to the extent necessary for the conclusion and execution of a contractual relationship, performing requests or for billing purposes. </p>
+    <p><strong>2.2. </strong>To protect your personally identifiable data collected and processed by us from unauthorized access and abuse, we have put a number of technical and organizational security measures in place, like encoding of privacy sensitive information (i.e. login data) transferred between this website and our servers.</p>
+    <p><strong>2.3. </strong>These security measures are monitored on a regular basis and updated according to technical progress as necessary. Our staff is well informed about privacy rules and obliged to follow them. </p>
+    <h3>3. Collection, Processing and Use of Personally Identifiable Data</h3>
+    <p><strong>3.1. </strong>Your personal data are used for the following purposes:</p>
+    <ul>
+      <li>The provision of our website and the acceptance, processing and execution of your orders.</li>
+      <li>Ensuring an efficient customer service and / or technical support for your products. When you contact us your details will be stored in order to process the enquiry and in case additional questions arise.</li>
+      <li>Technical notifications concerning our service.</li>
+    </ul>
+    <p><strong>3.2. </strong>We pass your data on to third parties where this is necessary for invoicing purposes (e.g. carrying out transactions) or for other reasons to enable us to fulfil our contractual obligations to you. Should such passing on not be clear to you, we will point it out to you.</p>
+    <h3>4. Newsletter</h3>
+    <p><strong>4.1. </strong>Newsletters and other emails that are not service-related, are not fulfilling our duties to perform our service and contain advertisement will only be send to you, if you have given us your prior consent.</p>
+    <p><strong>4.2. </strong>The content of the newsletter or email follows from its respective description. If no description is available, the content sent comprises information about us, our products and our services.</p>
+    <p><strong>4.3. </strong>If you no longer want to receive our newsletter or e-mails you can opt to stop receiving it at any time.</p>
+    <p><strong>4.4. </strong>You also give us the consent to store your IP addresses as part of the sign-up process. We are legally bound to record the log-on process so as to be able to verify that logging on is done in accordance with regulations.</p>
+    <h3>5. Cookies and similar features</h3>
+    <p><strong>5.1. </strong>A cookie (or similar data medium) is a small piece of data stored in a user's device while the user is using a website. Cookies can be send back to a server any time a user loads the website or accesses certain functions.</p>
+    <p><strong>5.2. </strong>We apply cookies only in order to support the functions of this website, optimize your experience as its user.</p>
+    <p><strong>5.3. </strong>We will ask for your consent or point out to the fact that we store and read cookies on your device.</p>
+    <p><strong>5.4. </strong>You may opt-out from many ad-serving cookies by using the services provided by http://www.aboutads.info/choices/ or http://www.youronlinechoices.com/uk/your-ad-choices/ for users form the European Union.</p>
+    <h3>6. Service and Notices</h3>
+    <p><strong>6.1. </strong>In compliance with legal requirements we will at any time provide you with information on your personally identifiable data stored by us, free of charge and without undue delay. </p>
+    <p><strong>6.2. </strong>Furthermore, you are free to revoke the approvals granted by confirming this Privacy Policy, at any time and with future effect. If you wish to do so, please feel free to contact us. Contact information is provided in Number 1 in this Privacy Policy.</p>
+    <h3>7. Amendments of this Privacy Policy</h3>
+    <p><strong>7.1. </strong>We reserve the right to modify or amend this Privacy Policy with view to the provision of new services, if and to the extent necessary. The same applies if the current Privacy Policy is found to contain any gaps. </p>
+    <p><strong>7.2. </strong>Upon considerable amendments and if your consent is needed, we will contact you by email sent to the email address provided to us.</p>
     <br><br>
   </div>
 </template>
