59f0f1a972fde01caa63cf96233387502a00d4d3
Till Kolter <till.kolter@gmail.com>
2017-09-11 16:10:13 +0200

    Add address and phone info

--- components/info/Questions.vue
+++ components/info/Questions.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <h3>Mail</h3>
+    <h2>Mail</h2>
     <p>If you have questions regarding an order please contact us: <a href="mailto:order@oye-records.com">order@oye-records.com</a></p>
+    <h2>Stores</h2>
+    <h3>OYE Records Prenzlauer Berg</h3>
+    <p>Oderbergerstr. 4<br/>10435 Berlin<br/>Germany</p>
+    <p>Phone: +49 (0)30 66647821</p>
+    <h3>OYE Records Neukölln</h3>
+    <p>Friedelstr. 49<br/>12047 Berlin<br/>Germany</p>
+    <p>Phone:  +49 (0)30 89372815</p>
   </div>
 </template>
 
