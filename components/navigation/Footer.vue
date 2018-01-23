b147313f086c3e1c4488e4df054619bdd8fdbc27
Till Kolter <till.kolter@gmail.com>
2018-01-23 18:26:57 +0100

    Add fluid footer

--- components/navigation/Footer.vue
+++ components/navigation/Footer.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="footer container-fluid">
-    <div class="logo-copyright">
-      &copy; 2017
-    </div>
-    <div class="terms">
-      <nuxt-link :to="{name: 'info-imprint'}">Imprint</nuxt-link>|
-      <nuxt-link :to="{name: 'info-terms-and-conditions'}">Terms</nuxt-link>|
-      <nuxt-link :to="{name: 'info-privacy'}">Privacy</nuxt-link>
-    </div>
-    <div class="creators">
-      <a href="mailto: buero@nanonansen.de">Design Nano Nansen</a>&nbsp;|
-      <a href="mailto: till.kolter@gmail.com">Development Till Kolter</a>
+  <div class="footer">
+    <div class="container-fluid footer-flex">
+      <div class="logo-copyright">
+        &copy; 2017
+      </div>
+      <div class="terms">
+        <nuxt-link :to="{name: 'info-imprint'}">Imprint</nuxt-link>|
+        <nuxt-link :to="{name: 'info-terms-and-conditions'}">Terms</nuxt-link>|
+        <nuxt-link :to="{name: 'info-privacy'}">Privacy</nuxt-link>
+      </div>
+      <div class="creators">
+        <a href="http://www.nanonansen.de">Design Nano Nansen</a>&nbsp;|
+        <a href="mailto: till.kolter@gmail.com">Development Till Kolter</a>
+      </div>
     </div>
   </div>
 </template>
