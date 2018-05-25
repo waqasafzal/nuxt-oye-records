a8126fcfe204e06435a6a3811ba682ea1b35ef44
Till Kolter <till.kolter@gmail.com>
2018-05-25 21:53:36 +0200

    Add mobile checkout login

--- components/checkout/CheckoutMethod.vue
+++ components/checkout/CheckoutMethod.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="row checkout__content checkout__method">
       <login-form redirect="#" class="checkout__content__col"></login-form>
-      <div class="col-12 col-md-6 login__form__panel checkout__content__col">
-        <!--<h3 class="checkout-box__header">Checkout as guest</h3>-->
-        <!--<div class="login__form">-->
-          <!--<proceed-button class="guest-btn" @click="setGuestCheckout">Continue as guest</proceed-button>-->
-        <!--</div>-->
+      <div class="d-none d-flex-md col-12 col-md-6 login__form__panel checkout__content__col">
       </div>
     </div>
 </template>
