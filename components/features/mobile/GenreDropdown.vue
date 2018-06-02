6cde151c9a5478505515518174d4d991af7ca777
Till Kolter <till.kolter@gmail.com>
2018-06-02 13:02:36 +0200

    Fix genre dropdown arrow

--- components/features/mobile/GenreDropdown.vue
+++ components/features/mobile/GenreDropdown.vue
@@ -124,6 +124,13 @@
         cursor: pointer;
       }
     }
+    .arrow {
+      border-width: 0 1px 1px 0 !important;
+      padding: 3px !important;
+      &-box {
+        margin-right: 0 !important;
+      }
+    }
   }
   .collapsed {
     display: none;
