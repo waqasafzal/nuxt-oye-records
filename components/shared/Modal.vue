d603b3404759a48ed5af9dfad5df68a837cf0c9f
Till Kolter <till.kolter@gmail.com>
2018-09-25 21:30:51 +0200

    Add reservation cancelation warningmodal

---
+++ components/shared/Modal.vue
@@ -0,0 +1,115 @@
+
+<template>
+  <div class="modal-backdrop">
+    <div class="oye-modal">
+      <header class="modal-header">
+        <slot name="headerTitle">
+          This is the default tile!
+        </slot>
+          <button
+              type="button"
+              class="btn-close"
+              @click="close"
+          >
+            x
+          </button>
+      </header>
+      <section v-if="showBody" class="modal-body">
+        <slot name="body">
+          I'm the default body!
+        </slot>
+      </section>
+      <footer class="modal-footer">
+        <slot name="footer">
+          I'm the default footer!
+
+          <button
+              type="button"
+              class="btn-green"
+              @click="close"
+          >
+            Close me!
+          </button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Modal',
+    props: {
+      showBody: true
+    },
+    methods: {
+      close () {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .oye-modal {
+    max-width: 360px;
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal-header,
+  .modal-footer {
+    padding: 15px;
+    display: flex;
+  }
+
+  .modal-header {
+    border-bottom: 1px solid #eeeeee;
+    color: #4AAE9B;
+    justify-content: space-between;
+    font-size: 18px;
+    line-height: 18px;
+  }
+
+  .modal-footer {
+    border-top: 1px solid #eeeeee;
+    justify-content: flex-end;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+
+  .btn-close {
+    border: none;
+    font-size: 20px;
+    /*padding: 20px;*/
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+
+  .btn-green {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #4AAE9B;
+    border-radius: 2px;
+  }
+</style>
