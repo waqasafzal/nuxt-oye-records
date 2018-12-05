97a4dea7a3d8d7265e97a82cdee7812db9675cda
Till Kolter <till.kolter@gmail.com>
2018-12-05 22:49:17 +0100

    Add Charts name

--- components/charts/ChartItem.vue
+++ components/charts/ChartItem.vue
@@ -5,12 +5,12 @@
     </div>
     <template v-if="chart.artist">
       <div class="charts-infobox__name">{{ chart.artist.name }}</div>
-      <template v-if="chart.name">{{ chart.name }}</template>
+      <template v-if="chart.name">{{ chart.name }} Charts</template>
       <div class="charts-infobox__label">{{ chart.artist.homeLabel }}</div>
     </template>
     <template v-else>
       <div class="charts-infobox__name">{{ chart.user.firstName }}</div>
-      <template v-if="chart.name">{{ chart.name }}</template>
+      <template v-if="chart.name">{{ chart.name }} Charts</template>
       <div class="charts-infobox__label">OYE Staff</div>
     </template>
   </nuxt-link>
