5562ae31e184108e948dea61c703ffb370375a6d
Till Kolter <till.kolter@gmail.com>
2019-01-13 21:09:50 +0100

    Remove spurious lines

--- plugins/apollo-config.js
+++ plugins/apollo-config.js
@@ -3,12 +3,10 @@ import {apiHost} from '../config/host'
 
 export default function(context) {
   return {
-    // required
     httpEndpoint: `${apiHost}/graphql`,
     httpLinkOptions: {
       credentials: 'same-origin'
     },
-    tokenName: 'apollo-token',
     validateToken: validateJwtToken
   }
 }
