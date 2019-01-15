77fef8e6f0c51177e42ce4c23d50b9c525611b17
Till Kolter <till.kolter@gmail.com>
2019-01-15 21:30:16 +0100

    Fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ module.exports = {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'keywords', name: 'keywords', content: 'vinyl,records,house,disco,jazz,techno,prenzlauer berg,berlin,neukölln,mailorder,germany'},
-      {name: 'robots', content: 'index, follow'},body-beat-ritual-december-2018-charts
+      {name: 'robots', content: 'index, follow'},
       {hid: 'description', name: 'description', content: 'Oye Records houses a fantastic range of music. Mostly devoted to house and disco there is also enough hip hop, jazz, and brand new electronic 12-inches.'},
       {
         hid: 'url',
