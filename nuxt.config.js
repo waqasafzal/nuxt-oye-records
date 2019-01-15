8247c7ed4d0264c10956e7cfb96dfe96b590d90e
Till Kolter <till.kolter@gmail.com>
2019-01-15 20:59:40 +0100

    Add HTML lang attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ module.exports = {
   },
   head: {
     title: 'OYE Records - Webshop',
+    lang: 'en',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
