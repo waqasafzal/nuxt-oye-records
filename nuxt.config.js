cfbc2a400c1c243346c687b69f6f1282b371c330
Till Kolter <till.kolter@gmail.com>
2019-01-15 21:01:09 +0100

    Push to http2

--- nuxt.config.js
+++ nuxt.config.js
@@ -198,6 +198,11 @@ module.exports = {
     // analyze: true
   },
   render: {
-    http2: {push: true}
+    http2: {
+      push: true,
+      pushAssets: (req, res, publicPath, preloadFiles) => preloadFiles
+        .filter(f => f.asType === 'script' && f.file === 'runtime.js')
+        .map(f => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`)
+    }
   }
 }
